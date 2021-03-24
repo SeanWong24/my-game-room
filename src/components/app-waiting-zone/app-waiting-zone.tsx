89cf8f906028ad5f4eba3042c449723407702de5
Sean Wong <seanwong24@live.com>
2021-03-24 04:37:19 +0000

    modify layout of app-waiting-zone

--- src/components/app-waiting-zone/app-waiting-zone.tsx
+++ src/components/app-waiting-zone/app-waiting-zone.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, ComponentInterface } from '@stencil/core';
+import { Component, Host, h, Prop, ComponentInterface, State } from '@stencil/core';
 import state from '../../utils/store';
 
 @Component({
@@ -7,6 +7,8 @@ import state from '../../utils/store';
   scoped: true,
 })
 export class AppWaitingZone implements ComponentInterface {
+
+  @State() viewSelection: string = 'chats';
 
   @Prop() roomName: string;
 
@@ -18,7 +20,7 @@ export class AppWaitingZone implements ComponentInterface {
             <ion-buttons slot="start">
               <ion-back-button default-href="/"></ion-back-button>
             </ion-buttons>
-            <ion-title>{this.roomName}</ion-title>
+            <ion-title>{this.roomName} - {state.players.length} player(s)</ion-title>
             <ion-buttons slot="end">
               <ion-menu-toggle>
                 <ion-button title="Player List">
@@ -29,26 +31,43 @@ export class AppWaitingZone implements ComponentInterface {
           </ion-toolbar>
         </ion-header>
 
-        <ion-split-pane contentId="chat-pane">
-          <ion-menu side="end" contentId="chat-pane">
+        <ion-split-pane contentId="main-view-container">
+          <ion-menu side="end" contentId="main-view-container">
             <ion-list>
               {
                 state.players?.map(({ name, isHost }, index) => (
                   <ion-item>
                     <ion-badge slot="start" color="primary">{index + 1}</ion-badge>
-                    <ion-label>{`${name}${isHost ? ' (Host)' : ''}`}</ion-label>
+                    <ion-label>{name}</ion-label>
                     {isHost && <ion-badge color="secondary">Host</ion-badge>}
                   </ion-item>
                 ))
               }
             </ion-list>
           </ion-menu>
-          <ion-content id="chat-pane" class="ion-padding">
-            Chat pane
+          <ion-content id="main-view-container" class="ion-padding">
+            <ion-segment value={this.viewSelection} onIonChange={({ detail }) => this.viewSelection = detail.value}>
+              <ion-segment-button value="chats">
+                <ion-label>Chats</ion-label>
+              </ion-segment-button>
+              <ion-segment-button value="games">
+                <ion-label>Games</ion-label>
+              </ion-segment-button>
+            </ion-segment>
+            {this.renderMainView()}
           </ion-content>
         </ion-split-pane>
       </Host>
     );
+  }
+
+  private renderMainView() {
+    switch (this.viewSelection) {
+      case 'chats':
+        return <div>Chat View</div>;
+      case 'games':
+        return <div>Game List</div>;
+    }
   }
 
 }
