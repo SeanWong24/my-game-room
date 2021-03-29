9ff4c2b7eb2cb983644692da4fca3519c447fb1a
Sean Wong <seanwong24@live.com>
2021-03-29 22:12:34 +0000

    add dark mode

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -7,6 +7,11 @@ import { games } from '../../utils/games';
   scoped: true,
 })
 export class AppRoot implements ComponentInterface {
+
+  connectedCallback() {
+    this.toggleDarkMode();
+  }
+
   render() {
     return (
       <ion-app>
@@ -23,4 +28,20 @@ export class AppRoot implements ComponentInterface {
       </ion-app>
     );
   }
+
+  private toggleDarkMode() {
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    toggleDarkTheme(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addEventListener('change', (mediaQuery) => toggleDarkTheme(mediaQuery.matches));
+
+    // Add or remove the "dark" class based on if the media query matches
+    function toggleDarkTheme(shouldAdd) {
+      document.body.classList.toggle('dark', shouldAdd);
+    }
+  }
+
 }
